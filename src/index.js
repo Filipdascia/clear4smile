5ba588bd179b0d6916b824cb3d5c03d085f03386
Filip <filip.dascia@gmail.com>
2025-05-25 20:15:27 +0200

    Create folder and file structure

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './styles/main.css';
+
+const root = document.getElementById('root');
+if (root) {
+  root.innerHTML = `
+    <h1>Welcome to Clear4Smile</h1>
+    <p>Your website is running without React.</p>
+  `;
+}
